aa61f3467199fc2da3e243928582a639f787a0df
phorton1 <available@upon.request>
2021-11-23 14:42:10 -0500

    added basic derived callbacks

--- bilgeAlarm2.ino
+++ bilgeAlarm2.ino
@@ -1,13 +1,40 @@
 #include <myDebug.h>
 #include <myIOTDevice.h>
 
+
+
+#define ONBOARD_LED             2
+
+
+#define DEFAULT_DISABLED            0          // enabled,disabled
+#define DEFAULT_BACKLIGHT_SECS      0          // off,secs
+#define DEFAULT_ERR_RUN_TIME        10         // off,secs
+#define DEFAULT_CRIT_RUN_TIME       30         // off,secs
+#define DEFAULT_ERR_PER_HOUR        2          // off,num
+#define DEFAULT_ERR_PER_DAY         12         // off,secs
+#define DEFAULT_EXTRA_RUN_TIME      5          // off,secs
+#define DEFAULT_EXTRA_RUN_MODE      1          // start, end, if primary_time
+#define DEFAULT_END_RUN_DELAY       2          // secs if mode=='end' and time != 0
+#define DEFAULT_RUN_EMERGENCY       30         // 255
+
+
 const IOTPreference_t bilge_prefs[] =
 {
-    { "NUM_RETRIES",    100,    PREFERENCE_TYPE_INT,    { .int_range = { 3, 0, 5}} },
-    { "NUM_PER_HOUR",   110,    PREFERENCE_TYPE_INT,    { .int_range = { 6, 1, 20}} },
+    { "DISABLED",         1000,    PREFERENCE_TYPE_INT,     { .int_range = { DEFAULT_DISABLED, 0, 1}} },
+    { "BACKLIGHT_SECS",   1010,    PREFERENCE_TYPE_INT,     { .int_range = { DEFAULT_BACKLIGHT_SECS, 0, 255}} },
+    { "ERR_RUN_TIME",     1020,    PREFERENCE_TYPE_INT,     { .int_range = { DEFAULT_ERR_RUN_TIME, 0, 255}} },
+    { "CRIT_RUN_TIME",    1030,    PREFERENCE_TYPE_INT,     { .int_range = { DEFAULT_CRIT_RUN_TIME, 0, 255}} },
+    { "ERR_PER_HOUR",     1040,    PREFERENCE_TYPE_INT,     { .int_range = { DEFAULT_ERR_PER_HOUR, 0, 255}} },
+    { "ERR_PER_DAY",      1050,    PREFERENCE_TYPE_INT,     { .int_range = { DEFAULT_ERR_PER_DAY, 0, 255}} },
+    { "EXTRA_RUN_TIME",   1060,    PREFERENCE_TYPE_INT,     { .int_range = { DEFAULT_EXTRA_RUN_TIME, 0, 255}} },
+    { "EXTRA_RUN_MODE",   1070,    PREFERENCE_TYPE_INT,     { .int_range = { DEFAULT_EXTRA_RUN_MODE, 0, 1}} },
+    { "END_RUN_DELAY",    1080,    PREFERENCE_TYPE_INT,     { .int_range = { DEFAULT_END_RUN_DELAY, 0, 255}} },
+    { "RUN_EMERGENCY",    1090,    PREFERENCE_TYPE_INT,     { .int_range = { DEFAULT_RUN_EMERGENCY, 0, 255}} },
 };
 
 #define NUM_BILGE_PREFS (sizeof(bilge_prefs)/sizeof(IOTPreference_t))
+
+
 
 class bilgeAlarm : public myIOTDevice
 {
@@ -16,10 +43,19 @@ public:
     bilgeAlarm()
     {
         addPrefs(bilge_prefs,NUM_BILGE_PREFS);
+        addTopic("esp32/output");
     }
     ~bilgeAlarm() {}
 
     virtual const char *getName() override  { return "bilgeAlarm"; }
+
+    virtual void onTopicMsg(String topic, String msg)
+    {
+        proc_entry();
+        display(0,"bilgeAlarm::onTopicMsg(%s,%s)",topic.c_str(),msg.c_str());
+        if (topic == "esp32/output")
+            digitalWrite(ONBOARD_LED,msg == "on" ? 1 : 0);
+    }
 
 };
 
@@ -34,14 +70,12 @@ void setup()
     display(0,"bilgeAlarm2.ino setup() started",0);
     proc_entry();
 
+    pinMode(ONBOARD_LED,OUTPUT);
+    digitalWrite(ONBOARD_LED,0);
+
     my_iot_device = new bilgeAlarm();
-
-    String s = my_iot_device->getPreferencesList();
-    Serial.print("bilgePrefs=");
-    Serial.println(s);
-
-
     my_iot_device->setup();
+
     proc_leave();
     display(0,"bilgeAlarm2.ino setup() completed",0);
 }
