4acc285eb45b14a1ac167e627d418cd2f0c27db6
phorton1 <available@upon.request>
2022-01-18 16:30:32 -0500

    implemented persistant SD run_history.dat file to work in conjunction with the RTC memory

--- baHistory.cpp
+++ baHistory.cpp
@@ -3,29 +3,106 @@
 //-----------------------------------
 // Interesting devlopement regarding the history in RAM.
 // I moved it to the RTC_NOINIT_ATTR memory and created
-// the onInitRTCMemory() virtual callchain from myIOTDevice::setup
-// when it needs to be re-initialied.  See myIOTDevice.cpp for
+// the onInitRTCMemory() virtual call chain from myIOTDevice::setup
+// when it needs to be re-initizalied.  See myIOTDevice.cpp for
 // more information.
 //
-// The history SHALL write a database to the SPIFFS (or SD) file system,
-// which appends each run as a 64 bit (8 byte) record to the file.
-// Upon the call to initRTCMemory(), if present, the last MAX_RUN_HISTORY
-// entries from that file shall be read into memory.
+// Then I added the WITH_HIST_DATABASE using the SD card to avoid
+// ever having to worry about running out of room.  On a soft-reboot
+// the history just stays in memory, but on a hard reboot, it is
+// read from disk in initRTCMemory().
 //
 // I am currently using 416.9K of 1.37M SPIFFS total, which leaves about 800K,
-// or enough room to store about 100,000 runs ... but for safety I would probably
-// keep 10,000 with a rotation scheme on SPIFFS.  Using the SDCard is another
-// option.
+// or enough room to store about 100,000 runs ... but for safety I would only
+// want to the last 10,000 or so, and would theoretically need a rotation scheme.
+// The SD Card is "better" in that it is not as likely to wear out, nor
+// be impacted by space considerations (like changing partition schemes, or
+// growth in the website), but is "worse" since it may not be present due to
+// the general weirdness with SD Cards.
+//
+// Having it in both locations (SD with fallback to SPIFFS) would also be
+// weird if the SD Card temporarily failed, then came back online.
+// Therefore it can only be in one place.
+//
+// So I chose the SDCard to start.  This depends on the SD Card
+// being initialized before the call to initRTCMemoory()
+//
+//--------------------------------------------------------------------------
+//
+// Upon the call to initRTCMemory(), if present, the last MAX_RUN_HISTORY
+// entries from that file are be read into memory.
+//
+// The runs are added to the database at endRun().   Note that bilgeAlarm.cpp
+// always ?!? sets flags between startRun() and endRun(), even though you see
+// the m_prev_run index being used for that purpopse, it is really the current
+// run.
+//
+//-------------------------------------------------------------------------
+//
+// There is one minor leftover wrinkle at this time.
+//
+// On soft-reboots the time_windows are retained, so you get
+// the behavior that clearing count errors at least temporarily
+// gives you a respite from an immediate alarm.  BUT if the machine
+// is hard-booted in a state where the last N entries constitute a
+// count error, the count error will be asserted immediately upon
+// reboot.   Oh well.  If really necessary, you can re-clear the error,
+// clear the history, or just wait an hour or day,
+//
+// Yhis new implementation also means that the HTML could be generated for
+// more runs than fit in memory (from the file) and/or in the LCD_UI but
+// would need backward traversal using seek, and that we probably want something
+// to clear less than the whole file (i.e. CLEAR BEFORE YEAR/MONTH)
+//
+//-------------------------------------------------------------------------
+//
+// Might be better to do away with the RTC memory stuff and just initialize
+// from the file if exists (and to write the time windows to files as they
+// are set too).   Change the order of #ifdefs so that #ifdef WITH_SD
+// drives which file system to use (and in general get rid of the WITH_SD
+// in myIOTSystem, making it either always on, or driven by a prefernce or
+// runtime init setting per-app).  Maybe do away with WITH_HIST_DATABASE.
+//
+// HOWEVER ... this is all good stuff, and right now it basically works, so
+// this is the implementation at the point of making the 1st circuit board(s).
 
 #include "baHistory.h"
 #include "bilgeAlarm.h"
 #include <myIOTLog.h>
+
+
+#define WITH_HIST_DATABASE  1
 
 #define DEBUG_HIST  0
 
 #define TEST_TIMES  0
     // define allows me to test in compressed time where
     // one hour == 1 minute, one day=3 minutes, and one week==5 minutes
+
+
+
+#if WITH_HIST_DATABASE
+    #define DATABASE_ON_SD 1        // as opposed to SPIFFS
+
+    #if DATABASE_ON_SD
+        #ifndef WITH_SD
+            #error baHistory::DATABASE_ON_SD without myIOTTypes::WITH_SD!!
+        #endif
+        #include <SD.h>
+        #define DBFS SD
+        #define FS_NAME "SD"
+    #else
+        #include <SPIFFS.h>
+        #define DBFS SPIFFS
+        #define FS_NAME "SPIFFS"
+    #endif
+
+    #define MAX_RUN_DB          10000    // 80K bytes
+    #define HIST_DB_FILENAME    "/run_history.dat"
+#endif
+
+
+
 
 
 time_t baHistory::m_start_duration;
@@ -37,7 +114,7 @@ RTC_NOINIT_ATTR runHistory_t baHistory::m_run_history[MAX_RUN_HISTORY];
 
 baHistory ba_history;
 
-void baHistory::clearHistory()
+void baHistory::init()
 {
     m_run_head = 0;
     m_prev_head = -1;
@@ -46,15 +123,105 @@ void baHistory::clearHistory()
     memset(m_run_history,0,MAX_RUN_HISTORY * sizeof(runHistory_t));
 }
 
+
+void baHistory::clearHistory()
+{
+    LOGI("baHistory::clearHistory()");
+    proc_entry();
+
+    #if WITH_HIST_DATABASE
+        #if DATABASE_ON_SD
+            if (!bilgeAlarm::hasSD())
+                LOGE("NO SD CARD in baHistory::clearHistory(DATABASE_ON_SD)");
+            else
+        #endif
+
+        if (DBFS.exists(HIST_DB_FILENAME))
+        {
+            LOGW("clearing baHistory from %s %s",FS_NAME,HIST_DB_FILENAME);
+            DBFS.remove(HIST_DB_FILENAME);
+        }
+        else
+        {
+            LOGD("Note: %s %s not found",FS_NAME,HIST_DB_FILENAME);
+        }
+    #endif
+
+    init();
+    proc_leave();
+}
+
+
+
 void baHistory::initRTCMemory()
 {
     LOGI("baHistory::initRTCMemory()");
-    m_run_head = 0;
-    m_prev_head = -1;
-    m_clear_day_time = 0;
-    m_clear_hour_time = 0;
-    memset(m_run_history,0,sizeof(m_run_history));
+    proc_entry();
+
+    init();
+
+    #if WITH_HIST_DATABASE
+        #if DATABASE_ON_SD
+            if (!bilgeAlarm::hasSD())
+                LOGE("NO SD CARD in baHistory::initRTCMemory(DATABASE_ON_SD)");
+            else
+        #endif
+
+        if (DBFS.exists(HIST_DB_FILENAME))
+        {
+            LOGW("initing baHistory from %s %s",FS_NAME,HIST_DB_FILENAME);
+            File file = DBFS.open(HIST_DB_FILENAME, FILE_READ);
+            if (!file)
+            {
+                LOGE("Could not open %s %s for reading",FS_NAME,HIST_DB_FILENAME);
+            }
+            else
+            {
+                uint32_t pos = 0;
+                uint32_t size = file.size();
+                uint32_t amt = size;
+                uint32_t max = (MAX_RUN_HISTORY-1) * sizeof(runHistory_t);
+                if (size > max)
+                {
+                    pos = size - max;
+                    amt = max;
+                }
+
+                LOGD("reading %d bytes from %s(%d) at pos=%d",amt,HIST_DB_FILENAME,size,pos);
+                if (!file.seek(pos))
+                {
+                    LOGE("Could not seek(%d) in %s %s",pos,FS_NAME,HIST_DB_FILENAME);
+                }
+                else
+                {
+                    uint32_t got = file.read((uint8_t *)&m_run_history, amt);
+                    if (got != amt)
+                    {
+                        LOGE("Error reading %s %s expected %d got %d",FS_NAME,HIST_DB_FILENAME,amt,got);
+                        init();
+                    }
+                    else
+                    {
+                        int num = amt/sizeof(runHistory_t);
+                        LOGD("got %d runs from %s %s",num,FS_NAME,HIST_DB_FILENAME);
+                        m_run_head = num;
+                    }
+                }
+                file.close();
+            }
+        }
+        else
+        {
+            LOGD("Note: %s %s not found",FS_NAME,HIST_DB_FILENAME);
+        }
+    #endif
+
+
+    proc_leave();
 }
+
+
+
 
 
 uint32_t baHistory::getStartDuration() const
@@ -99,6 +266,29 @@ uint32_t baHistory::endRun()
     runHistory_t *ptr = &m_run_history[m_prev_head];
     ptr->dur = duration;
     m_start_duration = 0;
+
+    #if WITH_HIST_DATABASE
+        #if DATABASE_ON_SD
+            if (!bilgeAlarm::hasSD())
+            {
+                LOGE("NO SD CARD in baHistory::endRun(DATABASE_ON_SD)");
+            }
+            else
+        #endif
+        {
+            File file = DBFS.open(HIST_DB_FILENAME, FILE_APPEND);
+            if (!file)
+            {
+                LOGE("Could not open %s %s for append",FS_NAME,HIST_DB_FILENAME);
+            }
+            else
+            {
+                file.write((const uint8_t *)ptr,sizeof(runHistory_t));
+                file.close();
+            }
+        }
+    #endif
+
     return duration;
 }
 
@@ -143,11 +333,6 @@ const runHistory_t *baHistory::getPrev(int *iterator) const
 
 
 int baHistory::countRuns(int count_how) const
-    // note that this does NOT count the current run which is happening
-    // it only counts fully commited runs, so per_day and per_hour alarms
-    // happen when the pump turns OFF.   Due to the alarm clearing strategy,
-    // the and week counts shown after a clear will more or less be zeroed,
-    // though the history still contains all the runs.
 {
     // LOGD("countRuns(%d) m_run_head=%d",hours,m_run_head);
     // can't really debug in here since it is called every second
@@ -165,7 +350,7 @@ int baHistory::countRuns(int count_how) const
         if (m_clear_day_time > cutoff)
             cutoff = m_clear_day_time;
     }
-    else // count_how == COUNT_HOUR
+    else if (count_how == COUNT_HOUR)
     {
         cutoff = now - (TEST_TIMES ? (1 * 60) : (60 * 60));
         if (m_clear_hour_time > cutoff)
@@ -217,10 +402,13 @@ void baHistory::setCountWindow(int type, int num)
 
 
 
-// might be a stack overflow
-// building this on the stack
+
 
 String baHistory::getHistoryHTML() const
+    // building this on the stack ?!?!?
+    // PRH - should test this with 256 entries
+    // would be better to be able to write this
+    // to the socket. web_server.streamFile(file, getContentType(path));
 {
     String rslt = "<head>\n";
     rslt += "<title>";
--- baHistory.h
+++ baHistory.h
@@ -6,13 +6,15 @@
 #include <Arduino.h>
 #include <time.h>
 
-
 #define MAX_RUN_HISTORY    256      // 256 * 8 = 2K
     // set to smaller number (i.e 15) for testing circular buffer
     //
     // circular buffer of run history, where each history element
     // is 8 bytes (two 32 bit numbers), the first being the time
-    // of the run, and the second being the duration of the run in seconds.
+    // of the run, and the second being the 16 bit duration of the
+    // run in seconds, and a 16 bit "flags" member that maps to the
+    // bilge alarm error STATE_CONSTANTS.
+    //
     // The buffer MUST be large enough to handle all the runs in a day,
     // or certainly at least more than the error configuration values
 
@@ -59,11 +61,14 @@ class baHistory
         void initIterator(int *iterator) const;
         const runHistory_t *getNext(int *iterator) const;
         const runHistory_t *getPrev(int *iterator) const;
-            // iterator over history
+            // iterator over history in memory
 
         String getHistoryHTML() const;
+            // delivers upto MAX_RUN_HISTORY lines of HTML
 
     private:
+
+        void init();
 
         static time_t m_start_duration;
         static RTC_NOINIT_ATTR int m_run_head;
@@ -72,7 +77,7 @@ class baHistory
         static RTC_NOINIT_ATTR time_t m_clear_hour_time;
         static RTC_NOINIT_ATTR runHistory_t m_run_history[MAX_RUN_HISTORY];
 
-};
+};  // class baHistory
 
 
 extern baHistory ba_history;
